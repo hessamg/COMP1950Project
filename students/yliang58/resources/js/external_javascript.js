b257baa62fcb28e9e3b9553b471777d6fae78b7c
yla249 <Didi@didis-mbp.wifi.bcit.ca>
2016-04-04 15:38:31 -0700

    2nd version

---
+++ students/yliang58/resources/js/external_javascript.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+
+	$("#nav").hide();
+	
+	
+	/*
+	if user clicks on the menu vector,
+	fade out the menu icon #nav_icon, 
+	slide open the full sized #nav
+	*/
+	$("#nav_icon").click(function(){
+		$("#nav").slideToggle();
+
+	
+	/*
+	if user clicks on the 'X' in the full sized menu,
+	slide away the full sized #nav
+	fade in the menu vector #nav_icon, 
+	*/
+
+	});
+
+});
